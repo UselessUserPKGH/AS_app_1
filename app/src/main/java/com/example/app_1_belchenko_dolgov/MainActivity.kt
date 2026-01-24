a245af0c31cf13b1a0fe37971038fb3a2cf2d6fb
Application_Belchenko <testylya333@gmail.com>
2026-01-24 11:21:00 +0300

    code fix

--- app/src/main/java/com/example/app_1_belchenko_dolgov/MainActivity.kt
+++ app/src/main/java/com/example/app_1_belchenko_dolgov/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.app_1_belchenko_dolgov
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.app_1_belchenko_dolgov.databinding.ActivityMainBinding
 import kotlin.random.Random
 
@@ -13,18 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
 
+        enableEdgeToEdge()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         setupButtonClickListener()
     }
 
     private fun setupButtonClickListener() {
         binding.button.setOnClickListener {
-
             val randomNumber = Random.nextInt(1, 7)
-
-
             val resultText = getString(R.string.dice_result_format, randomNumber)
             binding.label.text = resultText
         }
