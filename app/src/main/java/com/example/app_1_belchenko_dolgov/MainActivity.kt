912ab178d4f0c1e95e458a606aa01eaac9217fdf
Application_Belchenko <testylya333@gmail.com>
2026-01-31 09:33:20 +0300

    code optimization

--- app/src/main/java/com/example/app_1_belchenko_dolgov/MainActivity.kt
+++ app/src/main/java/com/example/app_1_belchenko_dolgov/MainActivity.kt
@@ -1,35 +1,21 @@
 package com.example.app_1_belchenko_dolgov
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.app_1_belchenko_dolgov.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-        enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         setupButtonClickListener()
     }
-
     private fun setupButtonClickListener() {
         binding.button.setOnClickListener {
             val randomNumber = Random.nextInt(1, 7)
