5fbdbb93e195017c086dda7626efa5bae337a577
Application_Belchenko <testylya333@gmail.com>
2026-01-31 10:30:58 +0300

    code optimization

--- app/src/main/java/com/example/app_1_belchenko_dolgov/MainActivity.kt
+++ app/src/main/java/com/example/app_1_belchenko_dolgov/MainActivity.kt
@@ -8,6 +8,7 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
